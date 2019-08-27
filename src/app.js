58eda2bcc3f3b8e130024cc669abdddd6c7f70e0
Daniel Ko <dankomong@gmail.com>
2019-08-26 20:27:52 -0400

    add query string

--- src/app.js
+++ src/app.js
@@ -40,16 +40,23 @@ app.get('/help', (req, res) => {
 })
 
 app.get('/weather', (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: 'You must provide an address!'
+    })
+  }
+
   res.send({
     forecast: 'It is snowing',
-    location: 'New York City'
+    location: 'New York City',
+    address: req.query.address
   })
 })
 
 app.get('/products', (req, res) => {
   if (!req.query.search) {
     return res.send({
-      error: 'You must provide a search term'
+      error: 'You must provide a search term!'
     })
   }
 
