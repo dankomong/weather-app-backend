0b96135f3e521e54fc30a6998a9289e4395b3ac9
Daniel Ko <dankomong@gmail.com>
2019-08-27 22:18:33 -0400

    add styling

--- public/js/app.js
+++ public/js/app.js
@@ -2,21 +2,26 @@ console.log('Client side javascript file is loaded!');
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
 
 weatherForm.addEventListener('submit', (event) => {
   event.preventDefault();
 
   const location = search.value;
 
+  messageOne.textContent = 'Loading...';
+  messageTwo.textContent = '';
+
   fetch(`http://localhost:3000/weather?address=${location}`).then(res => res.json())
   .then(data => {
     if (data.error) {
-      console.log(data.error);
+      messageOne.textContent = data.error;
     }
     else {
-      console.log(data.location);
-      console.log(data.forecast);
+      messageOne.textContent = data.location;
+      messageTwo.textContent = data.forecast;
     }
   })
-  
+
 })
