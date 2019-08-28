b8bfb4b49d16527fd6b6a2487a7254337d111dbd
Daniel Ko <dankomong@gmail.com>
2019-08-28 00:14:19 -0400

    add one more text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (lat, long, callback) => {
       callback('Unable to find location.')
     }
     else {
-      callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out.')
+      callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. The high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of ' + body.currently.precipType + '.' )
     }
   })
 }
